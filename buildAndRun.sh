8d5c60e3b04ccc7cd3f7f002d5a18c78ad659e52
NullCyan <gabrielthejota@gmail.com>
2025-09-13 14:14:24 -0300

    meh

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,14 +1,47 @@
 #!/bin/bash
-# buildAndRun.sh
+set -e
 
-# Optional: remove old build
-# rm -rf build
+# Check argument
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 [linux|windowsAndWine]"
+    exit 1
+fi
 
-cmake -S . -B build \
-  -DCMAKE_TOOLCHAIN_FILE=$HOME/vcpkg/scripts/buildsystems/vcpkg.cmake \
-  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+TARGET=$1
+BUILD_DIR="build_$TARGET"
 
-cmake --build build -j
+# Remove old build
+rm -rf "$BUILD_DIR"
+mkdir "$BUILD_DIR"
+cd "$BUILD_DIR"
 
-# Run the application
-./build/Application
+# Configure CMake depending on target
+if [ "$TARGET" == "linux" ]; then
+    cmake -S .. -B . \
+        -DCMAKE_TOOLCHAIN_FILE="$HOME/vcpkg/scripts/buildsystems/vcpkg.cmake" \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+elif [ "$TARGET" == "windowsAndWine" ]; then
+    cmake -S .. -B . \
+    -DCMAKE_TOOLCHAIN_FILE="$HOME/vcpkg/scripts/buildsystems/vcpkg.cmake" \
+    -DCMAKE_SYSTEM_NAME=Windows \
+    -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc \
+    -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ \
+    -DVCPKG_TARGET_TRIPLET=community/x64-windows-mingw \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+        
+
+else
+    echo "Unknown target: $TARGET"
+    exit 1
+fi
+
+# Build
+cmake --build . -j
+
+# Run
+if [ "$TARGET" == "linux" ]; then
+    ./Application
+elif [ "$TARGET" == "windowsAndWine" ]; then
+    wine ./Application.exe
+fi
--- clean.sh
+++ clean.sh
@@ -1,1 +1,2 @@
-rm -rf build/
+rm -rf build_linux
+rm -rf build_windowsAndWine
